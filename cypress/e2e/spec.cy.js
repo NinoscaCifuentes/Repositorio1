beb89a6b86ca14f13c47f407bc62b6a342d5421e
NinoscaCifuentes <104444981+NinoscaCifuentes@users.noreply.github.com>
2025-02-22 17:19:11 -0300

    common data

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,19 @@
 import {LoginElement} from "./pages/login/login.element";
 import {LoginMethonds} from "./pages/login/login.methonds";
 import {HomeMethonds} from "./home/home.methonds";
+import {Logger} from "./util/logger";
 
 describe('template spec', () => {
   it('passes', () => {
     // const username = 'ramdon1'
     // const password = 'ramdon1'
+    Logger.stepNumber(1)
+    Logger.step('Open the browser')
     cy.visit('https://demoblaze.com/')
-  HomeMethonds.clickOnProduct('Samsung galaxy s6')
+
+    Logger.stepNumber(2)
+    Logger.step('Click on login button')
+    HomeMethonds.clickOnProduct('Samsung galaxy s6')
 
   })
 })
